a472f06ca0ae622247f2bbf0d54548f85160dcb1
nero <resqnetm@gmail.com>
2022-04-17 16:47:31 +0300

    Add fade transition for icon

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fancyinput/fancyinput.dart';
 
-import 'package:flutter_svg/flutter_svg.dart';
-
 void main() {
   runApp(MaterialApp(
     home: Example(),
@@ -13,43 +11,36 @@ class Example extends StatelessWidget {
   final TextEditingController controller1 = TextEditingController();
   final TextEditingController controller2 = TextEditingController();
 
+  Example({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("FancyInput example"),
       ),
-
       body: Padding(
-        child: Column(
-          children: [
-            FancyInput(
-              style: FancyInputStyle.iOS(),
-              prefix: const Text("+7"),
-              suffix: const Icon(Icons.clear),
-
-              controller: controller1,
-              onTap: () { controller1.text = ""; },
-              keyboardType: TextInputType.number,
-              placeholder: "Hello iOS",
-            ),
-
-            const SizedBox(height: 16),
-            FancyInput(
-              style: FancyInputStyle.android(),
-              prefix: const Text("Test"),
-              suffix: const Icon(Icons.clear),
-
-              controller: controller2,
-              onTap: () { controller2.text = ""; },
-
-              placeholder: "Hello Android",
-            )
-          ]
-        ),
+        child: Column(children: [
+          FancyInput(
+            style: FancyInputStyle.iOS(),
+            prefix: const Text("+7"),
+            suffix: const Icon(Icons.clear),
+            controller: controller1,
+            keyboardType: TextInputType.number,
+            placeholder: "Hello iOS",
+            autofocus: true,
+          ),
+          const SizedBox(height: 16),
+          FancyInput(
+            style: FancyInputStyle.android(),
+            prefix: const Text("Test"),
+            suffix: const Icon(Icons.clear),
+            controller: controller2,
+            placeholder: "Hello Android",
+          )
+        ]),
         padding: const EdgeInsets.all(16),
       ),
-
       backgroundColor: const Color(0xffFFFFFF),
     );
   }
--- lib/src/fancyinput.dart
+++ lib/src/fancyinput.dart
@@ -1,60 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+class _InputSuffix extends StatefulWidget {
+  final FancyInputStyle style;
+  final Widget? suffix;
+  final bool enableSuffixFeedback;
+
+  final void Function()? onTap;
+  final void Function(String)? onChanged;
+
+  final FocusNode node;
+
+  final TextEditingController controller;
+
+  const _InputSuffix({
+    Key? key,
+    required this.style,
+    required this.suffix,
+    required this.enableSuffixFeedback,
+    required this.onTap,
+    required this.controller,
+    required this.onChanged,
+    required this.node,
+  }) : super(key: key);
+
+  @override
+  createState() => _InputSuffixState();
+}
+
+class _InputSuffixState extends State<_InputSuffix>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 200));
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+
+    _animationController.forward();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final style = widget.style;
+
+    return FadeTransition(
+      opacity: _animation,
+      child: InkWell(
+        child: SizedBox(
+          child: Container(
+            child: widget.suffix!,
+            alignment: Alignment.center,
+            width: style.iconSize.width,
+            height: style.iconSize.height,
+          ),
+          height: double.infinity,
+          width:
+              style.iconSize.width + style.padding.right + style.padding.left,
+        ),
+        enableFeedback: widget.enableSuffixFeedback,
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onTap: widget.onTap ??
+            () {
+              widget.controller.text = "";
+              if (widget.onChanged != null) {
+                widget.onChanged!("");
+              }
+            },
+      ),
+    );
+  }
+}
 
 class _FancyInput extends State<FancyInput> {
   late FocusNode node;
   late TextEditingController controller;
-
-  bool isSuffixShown()
-      => widget.suffix != null && (widget.suffixShowCondition == IconShowCondition.always || node.hasFocus);
+  final showSuffix = ValueNotifier<bool>(false);
 
   @override
   void initState() {
     node = widget.focusNode ?? FocusNode();
     controller = widget.controller ?? TextEditingController();
 
-    var lastValue = widget.autofocus;
-    node.addListener(() {
-      // Add state checking to reduce number of updates
-      if (node.hasFocus != lastValue) {
-        lastValue = node.hasFocus;
-        setState(() {});
-      }
-    });
+    if (widget.suffixShowCondition == IconShowCondition.focus) {
+      node.addListener(() {
+        // Add state checking to reduce number of updates
+        if (node.hasFocus != showSuffix.value) {
+          showSuffix.value = node.hasFocus;
+        }
+      });
+    }
 
     controller.text = widget.initialValue ?? "";
 
     super.initState();
-  }
-
-  Widget _buildSuffix() {
-    final style = widget.style;
-
-    return InkWell(
-      child: SizedBox(
-        child: Container(
-          child: widget.suffix!,
-          alignment: Alignment.center,
-
-          width: style.iconSize.width,
-          height: style.iconSize.height,
-        ),
-        height: double.infinity,
-        width: style.iconSize.width + style.padding.right + style.padding.left,
-      ),
-      enableFeedback: widget.enableSuffixFeedback,
-
-      splashColor: Colors.transparent,
-      highlightColor: Colors.transparent,
-
-      onTap: widget.onTap ?? () {
-        controller.text = "";
-        if (widget.onChanged != null) {
-          widget.onChanged!("");
-        }
-      },
-    );
   }
 
   @override
@@ -70,112 +114,99 @@ class _FancyInput extends State<FancyInput> {
     final doubleRadius = style.borderRadius;
 
     final radius = Radius.circular(doubleRadius);
-    final bottomRadius = Radius.circular(style.onlyTopRadius ? 0 : doubleRadius);
+    final bottomRadius =
+        Radius.circular(style.onlyTopRadius ? 0 : doubleRadius);
 
     var inputInsets = EdgeInsets.only(
       top: style.padding.top,
       bottom: style.padding.bottom,
     );
     final showDivider = widget.prefix != null && style.includeDivider;
-    final rightPadding = isSuffixShown() ? 0.0 : style.padding.right;
-
-    if (widget.prefix != null) {
-      inputInsets = inputInsets.copyWith(
-        left: style.dividerGap.width,
-        right: rightPadding
-      );
-    } else {
-      inputInsets = inputInsets.copyWith(
-        left: 16,
-        right: rightPadding
-      );
-    }
+    // final rightPadding = isSuffixShown() ? 0.0 : style.padding.right;
 
     return Container(
       child: IntrinsicHeight(
         child: Container(
           child: Row(
             children: [
-              if (widget.prefix != null) Container(
-                child: DefaultTextStyle(
-                  child: widget.prefix!,
-                  style: style.contentStyle.copyWith(color: style.prefixColor)
+              if (widget.prefix != null)
+                Container(
+                  child: DefaultTextStyle(
+                      child: widget.prefix!,
+                      style: style.contentStyle
+                          .copyWith(color: style.prefixColor)),
+                  margin: EdgeInsets.only(left: style.padding.left),
                 ),
-                margin: EdgeInsets.only(left: style.padding.left),
-              ),
               if (showDivider) SizedBox(width: style.padding.left),
-              if (showDivider) Padding(
-                child: VerticalDivider(
-                  color: style.dividerColor,
-                  width: style.dividerWeight,
-                ),
-
-                padding: EdgeInsets.symmetric(vertical: style.dividerGap.height),
-              ),
-
-              Expanded(
-                child: TextField(
-                  focusNode: node,
-                  controller: controller,
-                  autofocus: widget.autofocus,
-
-                  inputFormatters: widget.formatters,
-                  keyboardType: widget.keyboardType,
-                  
-                  onChanged: widget.onChanged,
-                  onSubmitted: widget.onSubmitted,
-                  onEditingComplete: widget.onEditingComplete,
-
-                  cursorColor: style.cursorColor,
-
-                  decoration: InputDecoration(
-                    contentPadding: inputInsets,
-                    isDense: true,
-
-                    hintText: widget.placeholder,
-                    hintStyle: style.contentStyle.copyWith(color: style.placeholderColor),
-
-                    border: InputBorder.none
+              if (showDivider)
+                Padding(
+                  child: VerticalDivider(
+                    color: style.dividerColor,
+                    width: style.dividerWeight,
                   ),
-
-                  style: style.contentStyle,
-                  textAlignVertical: TextAlignVertical.center,
+                  padding:
+                      EdgeInsets.symmetric(vertical: style.dividerGap.height),
+                ),
+              Expanded(
+                child: ValueListenableBuilder<bool>(
+                  builder: (_, value, __) => TextField(
+                    focusNode: node,
+                    controller: controller,
+                    autofocus: widget.autofocus,
+                    inputFormatters: widget.formatters,
+                    keyboardType: widget.keyboardType,
+                    onChanged: widget.onChanged,
+                    onSubmitted: widget.onSubmitted,
+                    onEditingComplete: widget.onEditingComplete,
+                    cursorColor: style.cursorColor,
+                    decoration: InputDecoration(
+                        contentPadding: inputInsets.copyWith(
+                            left: style.padding.left,
+                            right: value ? 0.0 : style.padding.right),
+                        isDense: true,
+                        hintText: widget.placeholder,
+                        hintStyle: style.contentStyle
+                            .copyWith(color: style.placeholderColor),
+                        border: InputBorder.none),
+                    style: style.contentStyle,
+                    textAlignVertical: TextAlignVertical.center,
+                  ),
+                  valueListenable: showSuffix,
                 ),
               ),
-
-              if (isSuffixShown()) _buildSuffix()
+              ValueListenableBuilder<bool>(
+                valueListenable: showSuffix,
+                builder: (_, value, __) => value
+                    ? _InputSuffix(
+                        style: style,
+                        suffix: widget.suffix,
+                        enableSuffixFeedback: widget.enableSuffixFeedback,
+                        onTap: widget.onTap,
+                        controller: controller,
+                        onChanged: widget.onChanged,
+                        node: node,
+                      )
+                    : Container(),
+              )
             ],
           ),
-          
           decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                color: style.underlineColor,
-                width: 1
-              )
-            )
-          ),
+              border: Border(
+                  bottom: BorderSide(color: style.underlineColor, width: 1))),
         ),
       ),
-
       decoration: BoxDecoration(
-        color: style.background,
-        borderRadius: BorderRadius.only(
-          topLeft: radius,
-          topRight: radius,
-
-          bottomLeft: bottomRadius,
-          bottomRight: bottomRadius
-        )
-      ),
+          color: style.background,
+          borderRadius: BorderRadius.only(
+              topLeft: radius,
+              topRight: radius,
+              bottomLeft: bottomRadius,
+              bottomRight: bottomRadius)),
     );
   }
 }
 
-enum IconShowCondition {
-  focus,
-  always
-}
+enum IconShowCondition { focus, always }
 
 class FancyInputStyle {
   final EdgeInsets padding;
@@ -204,7 +235,6 @@ class FancyInputStyle {
     required this.padding,
     required this.borderRadius,
     required this.dividerGap,
-
     required this.dividerColor,
     required this.background,
     required this.underlineColor,
@@ -212,69 +242,51 @@ class FancyInputStyle {
     required this.dividerWeight,
     required this.leftPrefixGap,
     required this.cursorColor,
-
     required this.placeholderColor,
     required this.contentStyle,
     required this.iconSize,
-
     this.onlyTopRadius = true,
     this.includeDivider = true,
   });
 
-  FancyInputStyle.android({
-    this.padding = const EdgeInsets.symmetric(vertical: 15, horizontal: 16),
-    this.background = const Color(0xffF0F0F0),
-    this.underlineColor = const Color.fromRGBO(38, 50, 56, 0.36),
-    this.dividerColor = const Color(0xffBEBEBE),
-    this.cursorColor = Colors.blue,
+  FancyInputStyle.android(
+      {this.padding = const EdgeInsets.symmetric(vertical: 15, horizontal: 16),
+      this.background = const Color(0xffF0F0F0),
+      this.underlineColor = const Color.fromRGBO(38, 50, 56, 0.36),
+      this.dividerColor = const Color(0xffBEBEBE),
+      this.cursorColor = Colors.blue,
+      this.placeholderColor = const Color(0xff868686),
+      this.onlyTopRadius = true,
+      this.includeDivider = true,
+      this.borderRadius = 4,
+      this.leftPrefixGap = 0,
+      this.iconSize = const Size(14, 14),
+      this.contentStyle = const TextStyle(
+          color: Color(0xff333333), fontSize: 16, letterSpacing: 0.44),
+      this.prefixColor = const Color(0xff868686),
+      this.dividerGap = const Size(12, 9),
+      this.dividerWeight = 2});
 
-    this.placeholderColor = const Color(0xff868686),
-
-    this.onlyTopRadius = true,
-    this.includeDivider = true,
-    this.borderRadius = 4,
-    this.leftPrefixGap = 0,
-
-    this.iconSize = const Size(14, 14),
-
-    this.contentStyle = const TextStyle(
-      color: Color(0xff333333),
-      fontSize: 16,
-      letterSpacing: 0.44
-    ),
-    this.prefixColor = const Color(0xff868686),
-
-    this.dividerGap = const Size(12, 9),
-    this.dividerWeight = 2
-  });
-
-  FancyInputStyle.iOS({
-    this.padding = const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-    this.leftPrefixGap = 4,
-    this.background = const Color(0xffF8F8F8),
-    this.underlineColor = Colors.transparent,
-    this.dividerColor = const Color(0xffBEBEBE),
-    this.cursorColor = Colors.blue,
-
-    this.onlyTopRadius = false,
-    this.includeDivider = true,
-    this.borderRadius = 18,
-
-    this.placeholderColor = const Color(0xff868686),
-
-    this.contentStyle = const TextStyle(
-      color: Color(0xff333333),
-      fontSize: 24,
-      letterSpacing: 0.37,
-      fontFamily: '.SF UI Display'
-    ),
-    this.prefixColor = const Color(0xff868686),
-
-    this.iconSize = const Size(13.35, 13.35),
-
-    this.dividerGap = const Size(12, 10),
-    this.dividerWeight = 2
-  });
+  FancyInputStyle.iOS(
+      {this.padding = const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+      this.leftPrefixGap = 4,
+      this.background = const Color(0xffF8F8F8),
+      this.underlineColor = Colors.transparent,
+      this.dividerColor = const Color(0xffBEBEBE),
+      this.cursorColor = Colors.blue,
+      this.onlyTopRadius = false,
+      this.includeDivider = true,
+      this.borderRadius = 18,
+      this.placeholderColor = const Color(0xff868686),
+      this.contentStyle = const TextStyle(
+          color: Color(0xff333333),
+          fontSize: 24,
+          letterSpacing: 0.37,
+          fontFamily: '.SF UI Display'),
+      this.prefixColor = const Color(0xff868686),
+      this.iconSize = const Size(13.35, 13.35),
+      this.dividerGap = const Size(12, 10),
+      this.dividerWeight = 2});
 }
 
 class FancyInput extends StatefulWidget {
@@ -310,37 +322,28 @@ class FancyInput extends StatefulWidget {
 
   final IconShowCondition suffixShowCondition;
 
-  const FancyInput({
-    Key? key,
-
-    required this.style,
-
-    this.prefix,
-    this.suffix,
-
-    this.onChanged,
-    this.onEditingComplete,
-    this.onSubmitted,
-    this.onSuffixTap,
-    this.onTap,
-
-    this.placeholder,
-
-    this.formatters,
-    this.keyboardType,
-
-    this.controller,
-    this.focusNode,
-    this.initialValue,
-
-    this.autocorrect = false,
-    this.autofocus = false,
-    this.enableSuffixFeedback = true,
-
-    this.suffixShowCondition = IconShowCondition.focus
-  }) : super(key: key);
+  const FancyInput(
+      {Key? key,
+      required this.style,
+      this.prefix,
+      this.suffix,
+      this.onChanged,
+      this.onEditingComplete,
+      this.onSubmitted,
+      this.onSuffixTap,
+      this.onTap,
+      this.placeholder,
+      this.formatters,
+      this.keyboardType,
+      this.controller,
+      this.focusNode,
+      this.initialValue,
+      this.autocorrect = false,
+      this.autofocus = false,
+      this.enableSuffixFeedback = true,
+      this.suffixShowCondition = IconShowCondition.focus})
+      : super(key: key);
 
   @override
   createState() => _FancyInput();
 }
-
